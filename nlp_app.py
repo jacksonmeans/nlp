4af7c4089d4820bb6620ce6ff7a025ad39b3a7ee
jacksonmeans <jrm380@case.edu>
2026-04-21 12:33:10 -0400

    Update nlp_app.py

--- nlp_app.py
+++ nlp_app.py
@@ -8,4 +8,30 @@ import re
 
 st.title('Sentiment Predictor')
 st.text_area('Input text')
-st.button('Analyze')
+if st.button('Analyze'):
+  #Keeping only Text and digits
+  text = re.sub(r"[^A-Za-z0-9]", " ", text)
+  #Removes Whitespaces
+  text = re.sub(r"\'s", " ", text)
+  # Removing Links if any
+  text = re.sub(r"http\S+", " link ", text)
+  # Removes Punctuations and Numbers
+  text = re.sub(r"\b\d+(?:\.\d+)?\s+", "", text)
+  # Splitting Text
+  text = text.split()
+  # Lemmatizer
+  lemmatizer = WordNetLemmatizer()
+  lemmatized_words = [lemmatizer.lemmatize(______) for word in text]
+      text = " ".join(_________)
+  blob = TextBlob(text)
+  sentiment_score = blob.sentiment.polarity
+  if result > 0:
+      custom_emoji = ':blush:'
+      st.success('Happy : {}'.format(______))
+  elif result < 0:
+      custom_emoji = ':disappointed:'
+      st.warning('Sad : {}'.format(_____))
+  else:
+      custom_emoji = ':confused:'
+      st.info('Confused : {}'.format(_____))
+  st.success("Polarity Score is: {}".format(result))
